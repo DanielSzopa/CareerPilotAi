3ea841916746b3f552f0e713ff758f36d390c7ea
Daniel <daniel.szopa333@gmail.com>
2025-05-06 07:22:13 +0200

    Add project files.

---
+++ CareerPilotAi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CareerPilotAi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
